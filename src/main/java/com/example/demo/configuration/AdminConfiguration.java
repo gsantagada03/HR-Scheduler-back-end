8bf209c4d3df19364068122ae9d317dee94700ee
Giuseppe Santagada <g.santagada03@gmail.com>
2024-08-08 14:01:36 +0200

    Commit 14:01 08/08/2024
     Implemented logic to require the admin to change the password on first login.
    
     Added a method to check if the password needs to be updated and to update the admin's password.

---
+++ src/main/java/com/example/demo/configuration/AdminConfiguration.java
@@ -0,0 +1,24 @@
+package com.example.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demo.model.Admin;
+import com.example.demo.repository.AdminRepository;
+
+@Configuration
+public class AdminConfiguration implements CommandLineRunner{
+	
+	@Autowired
+	private AdminRepository adminRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		if(adminRepository.count() == 0) {
+			Admin admin = new Admin("admin", "admin");
+			adminRepository.save(admin);
+		}
+	}
+	
+}
---
+++ src/main/java/com/example/demo/model/Admin.java
@@ -0,0 +1,27 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Admin {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	private String username;
+	private String password;
+	
+	public Admin(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+}
