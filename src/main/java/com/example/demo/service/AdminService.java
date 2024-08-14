2e3f6ed773443edaac8a050c5da4ab894ed355e6
Giuseppe Santagada <g.santagada03@gmail.com>
2024-08-14 11:45:34 +0200

    Commit 11:44 14/08/204

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -17,7 +17,6 @@ public class AdminService {
 	@Autowired
 	PasswordEncoder passwordEncoder;
 
-<<<<<<< Updated upstream
 	public boolean changePassword(String newPassword) {
 		final String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
 		Optional<Admin> admin = adminRepository.findByUsername("admin");
@@ -33,35 +32,13 @@ public class AdminService {
 	}
 
 	public boolean isFirstLogin() {
-=======
-	public void changePassword(String password) {
-		Optional<Admin> admin = adminRepository.findByUsername("admin");
-		if(admin.isPresent()) {
-			Admin adminObj = admin.get();
-			if(password != null) {
-				adminObj.setPassword(passwordEncoder.encode(password));
-				adminRepository.save(adminObj);
-			}
-		}
-	}
-
-	public boolean isFirstLogin(String password ) {
->>>>>>> Stashed changes
 		Optional<Admin> admin = adminRepository.findByUsername("admin");
 		if(admin.isPresent()) {
 			Admin adminObj = admin.get();
 			if(passwordEncoder.matches("admin", adminObj.getPassword())) { 
-<<<<<<< Updated upstream
-=======
-				changePassword(password);
->>>>>>> Stashed changes
 				return true;
 			}
 		}
 		return false;
 	}
 }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
